e89bf393f02fdca092d4fc099be2ed0948ef730a
rrf247082-netizen <rrf247082@gmail.com>
2026-08-04 02:35:49 +0530

    Refactor Firebase imports in script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-import { auth } from "./firebase.js";
 import {
+  auth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
-} from 
-   "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "./firebase.js";
 
 (() => {
   // ----- Storage helpers -----
